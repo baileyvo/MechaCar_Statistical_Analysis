ad3c90ef461ca7a223814968502a11adc8f75514
Van Ommeren <vanommeren.bailey@mayo.edu>
2022-04-17 20:40:21 -0500

    adding deliverable 3

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -30,3 +30,17 @@ lot_summary <- suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mea
                                                                              Median=median(PSI),
                                                                              Variance=var(PSI),
                                                                              SD=sd(PSI),.groups = 'keep')
+
+# Deliverable 3
+
+# 1 write an Rscript using the t.test function to determine if the PSI across all manufacturing lots is statistically different from the population mean of 1500 PSI
+t.test(suspension_coil$PSI,mu=1500)
+
+# 2 write three more RSripts using the ttest fuction and its subset arguement to determine if the PSI for each manufacturing lot is statistically different
+lot1 <- subset(suspension_coil, Manufacturing_Lot=="Lot1")
+lot2 <- subset(suspension_coil, Manufacturing_Lot=="Lot2")
+lot3 <- subset(suspension_coil, Manufacturing_Lot=="Lot3")
+
+t.test(lot1$PSI,mu=1500)
+t.test(lot2$PSI,mu=1500)
+t.test(lot3$PSI,mu=1500)
